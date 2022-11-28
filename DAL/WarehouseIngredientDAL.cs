5fe4746dc540c5314e732bb910c6c320348c81d7
toiQS <nguyensieu12112002@gmail.com>
2022-11-28 16:57:29 +0700

    upload

--- DAL/WarehouseIngredientDAL.cs
+++ DAL/WarehouseIngredientDAL.cs
@@ -11,25 +11,29 @@ namespace DAL
     public class WarehouseIngredientDAL
     {
         private FunctionsDAL funcDAL = new FunctionsDAL();
-        private string quety;
+        private string query;
         public DataTable getData()
         {
-            quety = string.Format("select * from WarehouseIngredient");
-            return funcDAL.getData(quety);
+            query = string.Format("select * from WarehouseIngredient");
+            return funcDAL.getData(query);
         }
 
         public bool insertData(WarehouseIngredientDTO dataDTO)
         {
-            quety = string.Format("insert into WarehouseIngredient(idFood, Status, WarehouseDayAdd, idRequireAdd, idStafftoAddWarehouse) values('{0}','{1}','{2}','{3}','{4}')",
+            query = string.Format("insert into WarehouseIngredient(idFood, Status, WarehouseDayAdd, idRequireAdd, idStaffAddtoWarehouse) values('{0}','{1}','{2}','{3}','{4}')",
                 dataDTO.IdFood, dataDTO.Status, dataDTO.WarehouseDayAdd, dataDTO.IdRequireAdd, dataDTO.IdStaffAddtoWarehouse);
-            return funcDAL.func(quety, dataDTO);
+            return funcDAL.func(query, dataDTO);
         }
 
         public bool updateData(WarehouseIngredientDTO dataDTO)
         {
-            quety = string.Format("update WarehouseIngredient set idFood = {0}, Status = '{1}', WarehouseDayAdd = '{2}', idRequireAdd = '{3}', idStafftoAddWarehouse = '{4}'", 
+            query = string.Format("update WarehouseIngredient set idFood = {0}, Status = '{1}', WarehouseDayAdd = '{2}', idRequireAdd = '{3}', idStaffAddtoWarehouse = '{4}'", 
                 dataDTO.IdFood, dataDTO.Status, dataDTO.WarehouseDayAdd, dataDTO.IdRequireAdd, dataDTO.IdStaffAddtoWarehouse);
-            return funcDAL.func(quety, dataDTO);
+            return funcDAL.func(query, dataDTO);
+        }
+        public bool delData(WarehouseIngredientDAL dataDTO)
+        {
+            return false;
         }
     }
 }
