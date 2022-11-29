ba3a2291000a7ab2caf884cda9bdf8787b4715f6
toiQS <nguyensieu12112002@gmail.com>
2022-11-29 08:17:04 +0700

    a

--- DAL/WarehouseIngredientDAL.cs
+++ DAL/WarehouseIngredientDAL.cs
@@ -20,7 +20,7 @@ namespace DAL
 
         public bool insertData(WarehouseIngredientDTO dataDTO)
         {
-            query = string.Format("insert into WarehouseIngredient(idFood, Status, WarehouseDayAdd, idRequireAdd, idStaffAddtoWarehouse) values('{0}','{1}','{2}','{3}','{4}')",
+            query = string.Format("insert into WarehouseIngredient(idFood, Status, WarehouseDayAdd, idRequireAdd, idStaffAddtoWarehouse) values({0},'{1}','{2}','{3}','{4}')",
                 dataDTO.IdFood, dataDTO.Status, dataDTO.WarehouseDayAdd, dataDTO.IdRequireAdd, dataDTO.IdStaffAddtoWarehouse);
             return funcDAL.func(query, dataDTO);
         }
